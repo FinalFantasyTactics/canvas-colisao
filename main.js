81abc269eec4128fb44a7c0c5f6688f2f4afac92
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-03 11:16:22 -0300

    add movimentação ao objeto

--- main.js
+++ main.js
@@ -9,20 +9,67 @@ var posY = 50;
 var objColor = "#00F00F";
 var mvLeft = mvUp = mvRight = mvDown = false;
 
+function updateBlock() {
+    if (mvLeft) {
+        posX--;
+    }
+    if (mvRight) {
+        posX++;
+    }
+    if (mvUp) {
+        posY--;
+    }
+    if (mvDown) {
+        posY++;
+    }
+}
 
 // Captura o comando
 window.addEventListener("keydown", keydownHandler, false);
+window.addEventListener("keyup", keyupHandler, false);
 
 function keydownHandler(e) {
-    alert(e.keyCode);
+    var key = e.keyCode;
+    switch (key) {
+        case UP:
+            mvUp = true;
+            break;
+        case DOWN:
+            mvDown = true;
+            break;
+        case LEFT:
+            mvLeft = true;
+            break;
+        case RIGHT:
+            mvRight = true;
+            break;
+    }
+}
+
+function keyupHandler(e) {
+    var key = e.keyCode;
+    switch (key) {
+        case UP:
+            mvUp = false;
+            break;
+        case DOWN:
+            mvDown = false;
+            break;
+        case LEFT:
+            mvLeft = false;
+            break;
+        case RIGHT:
+            mvRight = false;
+            break;
+    }
 }
 
 function update() {
-
+    updateBlock();
 }
 
 function draw() {
-    ctx.fillRect(50, 50, 50, 50);
+    ctx.fillRect(posX, posY, SIZE, SIZE);
 }
 
 // atualiza a frame
