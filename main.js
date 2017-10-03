b87ed9747b93a54d895df53af1a8fad89f6f1db9
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-03 11:04:58 -0300

    add constants

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var cnv = document.querySelector("canvas");
 var ctx = cnv.getContext("2d");
 
+// constatntes
+var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+var SIZE = 50;
+var posX = 50;
+var posY = 50;
+var objColor = "#00F00F";
+var mvLeft = mvUp = mvRight = mvDown = false;
+
+
 // Captura o comando
 window.addEventListener("keydown", keydownHandler, false);
 
